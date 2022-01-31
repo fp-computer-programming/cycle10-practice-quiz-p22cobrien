3319c70693409c78510197fc67d58a23c34ac66a
p22cobrien <p22cobrien@fairfieldprep.org>
2022-01-31 10:31:01 -0500

    complete #2

--- cycle-10_practice-quiz_2.py
+++ cycle-10_practice-quiz_2.py
@@ -1,2 +1,15 @@
 # Author: CMOB 1/27/2022
 
+def factorial(num):
+    sum = ""
+    index = 0
+    while index  <= num - 1:
+        sum += (str(index + 1))
+        index += 1
+
+    print(sum)
+
+
+factorial(5)
+factorial(88)
+
